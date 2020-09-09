89d782d0dc28c07672b6a77ce48024c9fa2d9561
Bruno Santos <brunolssantos@hotmail.com>
2020-09-09 20:54:58 -0300

    atualização menor

---
+++ scripts/addField.js
@@ -0,0 +1,13 @@
+//procurar o botão
+
+document.querySelector("#add-time")
+.addEventListener('click',cloneField)
+// quando clicar no botao
+
+//executar uma ação
+function cloneField(){
+    console.log("Cheguei aqui")
+}
+    //duplicar os campos
+
+    // colocar na pagina
