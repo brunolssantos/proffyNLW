5437a4661422b7e09f2dd076b4e22ffcd9c12442
Bruno Santos <brunolssantos@hotmail.com>
2020-09-10 20:43:51 -0300

    adding first javascript file for give-classes form

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,13 +1,29 @@
 //procurar o botão
 
 document.querySelector("#add-time")
-.addEventListener('click',cloneField)
 // quando clicar no botao
+.addEventListener('click',cloneField);
+
 
 //executar uma ação
 function cloneField(){
-    console.log("Cheguei aqui")
-}
-    //duplicar os campos
+    //duplicar os campos. que  campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true); //boolean: true ou false
+    
+    //pegar os campos. Que campos?
 
-    // colocar na pagina
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    //para cada campo, limpar.
+    fields.forEach(function(field){
+        //pegar o field do momento e limpa ele
+        field.value = "";
+    })
+
+    fields[0].value = "";
+    fields[1].value = "";
+
+
+    // colocar na pagina. onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer);
+}
